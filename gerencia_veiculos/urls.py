b7238b98e1ceff7f225820bbf5ac9b681271b65d
Felipe Oliveira Colen <felipecolen@gmail.com>
2017-05-08 15:23:44 -0400

    ajustando namespace

--- gerencia_veiculos/urls.py
+++ gerencia_veiculos/urls.py
@@ -1,4 +1,4 @@
-"""GerenciamentoDeVeiculos URL Configuration
+"""gerencia_veiculos URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
